2cdb912c4cc24ec50d86ae073076e423010a1941
Matheus Sousa <ma.sousa.soares@gmail.com>
2023-06-19 23:39:16 -0300

    Modal Editar endereço - Incompleto

--- src/DevMS.App/Controllers/FornecedoresController.cs
+++ src/DevMS.App/Controllers/FornecedoresController.cs
@@ -101,6 +101,18 @@ namespace DevMS.App.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> AtualizarEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if(fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+        }
+
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id)); 
