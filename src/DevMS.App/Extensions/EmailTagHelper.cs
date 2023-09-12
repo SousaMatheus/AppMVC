e75854875fa67a5a2219661032f92dc6ecc23f9f
Matheus Sousa <ma.sousa.soares@gmail.com>
2023-09-11 23:18:06 -0300

    Ajustes Views - Footer, Index e EmailTagHelper

--- src/DevMS.App/Extensions/EmailTagHelper.cs
+++ src/DevMS.App/Extensions/EmailTagHelper.cs
@@ -4,7 +4,7 @@ namespace DevMS.App.Extensions
 {
     public class EmailTagHelper :TagHelper
     {
-        public string EmailDomain { get; set; } = "msdev.com.br";
+        public string EmailDomain { get; set; } = "gmail.com";
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
